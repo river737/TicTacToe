2f6c0d489597e2fda55c60814bb8d20529b24311
sophearom chea <10spr@sophearoms-MacBook-Pro.local>
2021-08-07 21:30:50 +0700

    Finished with the initial socket events and data structure

--- src/pages/api/socket.js
+++ src/pages/api/socket.js
@@ -2,14 +2,32 @@
 
 import {Server} from 'socket.io'
 
+import SocketFunctions from '../../socket/socketFunctions'
+
 export default (req, res)  => {
   if(!res.socket?.server?.io) {
-    console.log("socket server initialized")
+    console.log("Socket Server Initialized")
     const socketServer = res.socket.server
     const io = new Server(socketServer, {path: '/api/socket'})
     io.on('connection', socket => {
-      console.log(socket.id)
+      SocketFunctions({io, socket})
     })
+    io.data = {users: {id: [], username: []}, rooms: {}}
+
+    /* 
+      rooms: {
+        "unique_room_id": {
+          config: {
+            private: true
+          },
+          players: {
+            "socket_id": {
+              
+            }
+          }
+        }
+      }
+    */
     res.socket.server.io = io
   }
   res.end()
