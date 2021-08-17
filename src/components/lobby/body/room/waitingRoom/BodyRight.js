7b6b5129adce7a57a705e5940c6c9050c254653d
sophearom chea <10spr@sophearoms-MacBook-Pro.local>
2021-08-17 10:30:34 +0700

    Added copy text function

--- src/components/lobby/body/room/waitingRoom/BodyRight.js
+++ src/components/lobby/body/room/waitingRoom/BodyRight.js
@@ -2,6 +2,14 @@
 import styles from '../../../../../../styles/lobby/body/room/waitingRoom/BodyRight.module.css'
 
 export default function BodyRight({room}) {
+    const copy = async () => {
+        try {
+            await navigator.clipboard.writeText(room.id)
+        }
+        catch(err) {
+            console.log('copy to clipboard failed', err)
+        }
+    }
     return (
 
         <div className={styles.bodyRight}>
@@ -9,7 +17,7 @@ export default function BodyRight({room}) {
                 <span className={styles.widgetTitle}>Invite Players</span>
                 <div className={styles.textBox}>
                     <span className={styles.left}>{room.id}</span>
-                    <button>
+                    <button onClick={copy}>
                         <i className="fa fa-copy" />
                     </button>
                 </div>
