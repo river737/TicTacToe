0e0485781b22e94f9aa574b315a46496532353b3
river737 <=>
2021-08-31 23:54:37 +0700

    debug 4

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -8,29 +8,22 @@ export default function handle(req, res) {
 
 function post(req, res) {
     let users = store.get('users')
-    if(!users) users = 'undefined'
-    let pusher = res?.socket?.server?.pusher ? 'ok' : 'undefined'
-    
-    const obj = {success: true, data: {users, pusher}}
-    
 
-    res.json(obj)
+    const {socketId, data} = req.body
+    const {username, check} = data
+    const success = () => {
+        storeObject.update({name: 'users', value: {username}, keys: [socketId]})
+        res?.setHeader('Set-Cookie',`username=${username}; path=/; Max-Age=86400; HttpOnly, SameSite=Strict`);
 
-    // const {socketId, data} = req.body
-    // const {username, check} = data
-    // const success = () => {
-    //     storeObject.update({name: 'users', value: {username}, keys: [socketId]})
-    //     res?.setHeader('Set-Cookie',`username=${username}; path=/; Max-Age=86400; HttpOnly, SameSite=Strict`);
-
-    //     res.json({success: true})
-    // }
-    // if(check) {
-    //     if(Object.keys(users).filter(id => users[id].username === username).length === 0) {
-    //         success()
-    //     } else {
-    //         res.json({success: false, error: {msg: "Username is taken"}})
-    //     }
-    // } else {
-    //     success()
-    // }
+        res.json({success: true})
+    }
+    if(check) {
+        if(Object.keys(users).filter(id => users[id].username === username).length === 0) {
+            success()
+        } else {
+            res.json({success: false, error: {msg: "Username is taken"}})
+        }
+    } else {
+        success()
+    }
 }
